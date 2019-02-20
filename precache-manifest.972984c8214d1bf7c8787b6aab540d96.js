b8bc896a58f5125436120ad4a7a48ceadd795c00
wala1a <walaa_almutairi@hotmail.com>
2019-02-20 16:06:45 +0300

    Updates

---
+++ precache-manifest.972984c8214d1bf7c8787b6aab540d96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "418f4b539fd1815d8484",
+    "url": "/todolist/static/css/main.31635e5c.chunk.css"
+  },
+  {
+    "revision": "418f4b539fd1815d8484",
+    "url": "/todolist/static/js/main.418f4b53.chunk.js"
+  },
+  {
+    "revision": "a87775f5415b47ec7ede",
+    "url": "/todolist/static/js/runtime~main.a87775f5.js"
+  },
+  {
+    "revision": "f1320c0db08a9d99840a",
+    "url": "/todolist/static/js/2.f1320c0d.chunk.js"
+  },
+  {
+    "revision": "8985b5d15bd682b1b2838b3e738a757e",
+    "url": "/todolist/static/media/background.8985b5d1.jpg"
+  },
+  {
+    "revision": "f39948db3f35f467a3fe81736a79d7ca",
+    "url": "/todolist/index.html"
+  }
+];
